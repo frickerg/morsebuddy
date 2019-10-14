8abe581c92c4c4b74de839a57922af1ef7a84fb6
Guillaume Fricker <fricker.g@zoho.com>
2019-10-14 03:43:30 +0200

    Improved synthbeep with function call

--- buddy.sh
+++ buddy.sh
@@ -1,23 +1,31 @@
 #sudo apt-get install sox || brew install sox
-d4='play -q -n synth 0.1 sin 293.66'
-d5='play -q -n synth 0.1 sin 587.33'
-a4='play -q -n synth 0.1 sin 440'
-gs4='play -q -n synth 0.1 sin 415.30'
-g4='play -q -n synth 0.1 sin 392'
-f4_long='play -q -n synth 0.2 sin 349.23'
-f4='play -q -n synth 0.1 sin 349.23'
 
-shortBeep='play -q -n synth 0.1 sin 880'
-longBeep='play -q -n synth 0.5 sin 420'
-okayBeep='play -q -n synth 0.1 sin 560'
-lowBeep='play -q -n synth 2 sin 493.88'
+function synthbeep {
+	if [ -z "$2" ]
+	then
+		play -q -n synth 0.1 sin $1
+	else
+		play -q -n synth $1 sin $2
+	fi
+}
 
+d4='synthbeep 293.66'
+d5='synthbeep 587.33'
+a4='synthbeep 440'
+gs4='synthbeep 415.30'
+g4='synthbeep 392'
+f4_long='synthbeep 0.2 349.23'
+f4='synthbeep 349.23'
+
+shortBeep='synthbeep 0.1 880'
+longBeep='synthbeep 0.5 420'
 
 echo 'testing'
 
+# play testing sound
 $d4 && $d4 && $d5 && sleep 0.1 && $a4 && sleep 0.2
 $gs4 && sleep 0.1 && $g4 && sleep 0.1 && $f4_long && $d4 && $f4 && $g4
 
-sleep 1 && clear
+clear
 echo 'ready!'
 sleep 1 && clear
