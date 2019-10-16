16684b88f3f3420402bd64019616859e2488cb89
Guillaume Fricker <fricker.g@zoho.com>
2019-10-16 10:39:33 +0200

    Code and param cleanup

--- buddy.sh
+++ buddy.sh
@@ -3,7 +3,12 @@
 # author: frickerg
 
 # update dependencies
-# sudo apt-get install sox || brew install sox
+# for using the soundcard, sox needs to be installed
+# if you do not want to use any external packages, use the param --mute when executing the script
+# using the param --update will install/update the external packages (optional, but much more fun!)
+play_sound="true"
+if [ "$1" == "--mute" ];then play_sound="false" && printf "you have chosen to disable the sound function.\nno external packages will be used.\n";fi
+if [ "$1" == "--update" ];then echo "updating external packages" && sudo apt-get install sox || brew install sox; fi
 
 # execute script in the project folder
 cd "$(dirname "$0")"
@@ -21,26 +26,25 @@ banner[8]="+-------------------------------+-----------------------------+------
 banner[9]="|  Developed by G. Fricker      |       Follow me on GitHub:  |   @frickerg   |"
 banner[10]="+-------------------------------+-----------------------------+---------------+"
 
+# animate the banner
 function banner {
-	for i in "${banner[@]}"
-	do
-		echo "$i"
-		sleep 0.1
-	done
+	for i in "${banner[@]}";do echo "$i" && sleep 0.1;done
 }
 
 # read resource file to know the morse alphabet
 while read -r line; do declare  "morse_$line"; done <resources/alphabet.res
 
 # plays a tune on the sound card
-# param $1: frequency
-# param $2: duration (optional)
+# param $1: frequency, param $2: duration (optional)
 function synthbeep {
-	if [ -z "$2" ]
-	then
-		play -q -n synth 0.1 $mode $1
-	else
-		play -q -n synth $2 $mode $1
+	if [ $play_sound == "true" ];then
+		if [ -z "$2" ];then
+			# only use one argument
+			play -q -n synth 0.1 $mode $1
+		else
+			# use the second argument as beep length
+			play -q -n synth $2 $mode $1
+		fi
 	fi
 }
 
@@ -55,48 +59,38 @@ f4='synthbeep 349.23'
 
 # define the notes for the morse sounds
 shortBeep='synthbeep 880'
-longBeep='synthbeep 420 0.5'
+longBeep='synthbeep 560 0.5'
 
 # set synth mode to triangle
 mode=triangle
-clear
-sleep 0.2
+clear && sleep 0.2
 
 # play intro song and display banner
 banner & ($d4 && $d4 && $d5 && sleep 0.1 && $a4 && sleep 0.2 && $gs4 && sleep 0.1 && $g4 && sleep 0.1 && $f4_long && $d4 && $f4 && $g4)
-sleep 1
-echo 'ready to go!'
-
+sleep 1 && printf 'Ready to go, you can exit anytime by simply typing "exit"\nHow does morse work? Check out the PDF in the "manual" folder.\nType in lower case and morsebuddy will translate it into morse code for you!\n\n'
 # set synth mode to sinus
 mode=sin
 
+# the decoding function to display text in morse code. also plays the beeping sound if sox is installed
 function decode {
 	local var="morse_$1"
 	local decode_letter="${!var}"
-	for (( j=0; j<${#decode_letter}; j++ ))
-	do
-		if [[ ${decode_letter:$j:1} == "." ]]
-		then
-			$shortBeep
-			echo -n "."
+	for (( j=0; j<${#decode_letter}; j++ ));do
+		if [[ ${decode_letter:$j:1} == "." ]];then
+			$shortBeep && echo -n "."
 		else
-			$longBeep
-			echo -n "-"
+			$longBeep && echo -n "-"
 		fi
 	done
 }
 
-while [[ $userinput != "exit" ]];
-do
+while true;do
 	read -p 'Enter your text message: ' userinput
-	for (( i=0; i<${#userinput}; i++ )); do
-		current_letter=${userinput:$i:1}
-		decode $current_letter
-		if [[ "$current_letter" == " " ]]
-		then
-			echo -n "/"
-		fi
-		echo -n " "
-		sleep 1
+	if [[ $userinput == "exit" ]];then sleep 1 && printf "Thank you and have a --. .-. . .- - day!\n\n" && exit 1;fi
+	for (( i=0; i<${#userinput}; i++ ));do
+		current_letter=${userinput:$i:1} && decode $current_letter
+		if [[ "$current_letter" == " " ]];then echo -n "/";fi
+		echo -n " " && sleep 1
 	done
+	printf "\n\n"
 done
